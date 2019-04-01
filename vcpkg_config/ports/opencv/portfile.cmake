529c217e3243f24ba141836af4bdd7e31ce6fd8a
kiwixz <kiwixz@outlook.com>
2019-04-01 17:46:32 +0200

    add videoio module to opencv

--- vcpkg_config/ports/opencv/portfile.cmake
+++ vcpkg_config/ports/opencv/portfile.cmake
@@ -17,8 +17,7 @@ vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        -DBUILD_LIST=core
-        #-DBUILD_LIST=videoio
+        -DBUILD_LIST=videoio
         -DWITH_JASPER=OFF
         -DWITH_JPEG=OFF
         -DWITH_OPENEXR=OFF
