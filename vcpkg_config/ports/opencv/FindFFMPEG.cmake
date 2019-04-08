e0bdd25d5fed24e19e9360ed34a25048106d0a13
kiwixz <kiwixz@outlook.com>
2019-04-08 10:09:21 +0100

    fix findffmpeg cmake

--- vcpkg_config/ports/opencv/FindFFMPEG.cmake
+++ vcpkg_config/ports/opencv/FindFFMPEG.cmake
@@ -2,12 +2,12 @@ include(FindPackageHandleStandardArgs)
 
 find_path(FFMPEG_INCLUDE_DIRS NAMES libavcodec/avcodec.h)
 unset(FFMPEG_LIBRARIES)
-foreach (FFMPEG_SUBLIBRARY avformat avdevice avcodec avutil swscale)
+foreach (FFMPEG_SUBLIBRARY avcodec avdevice avfilter avformat avutil swresample swscale)
     find_library(FFMPEG_lib${FFMPEG_SUBLIBRARY}_LIBRARY NAMES ${FFMPEG_SUBLIBRARY})
     list(APPEND FFMPEG_LIBRARIES ${FFMPEG_lib${FFMPEG_SUBLIBRARY}_LIBRARY})
 endforeach ()
 if (WIN32)
-    list(APPEND FFMPEG_LIBRARIES wsock32 ws2_32 Secur32)
+    list(APPEND FFMPEG_LIBRARIES onecore)
 endif ()
 
 include_directories(SYSTEM ${FFMPEG_INCLUDE_DIRS})  # OpenCV 4.0.1 need this, seems fixed on master
