8c47c58a525f665c4ac4c67d6d439bf354309191
ci <ci>
2019-06-26 18:42:27 +0000

    [ci] update 'doc/master' commit d9600bb5a6bf97e9c36c20ab0b0d5bc3c7819d7d

--- doc/master/search/variables_2.js
+++ doc/master/search/variables_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['farneback_5f',['farneback_',['../structsoe_1_1_frame_stream_cuda.html#a272e3fc6ddbf079f1458f6f3fee01540',1,'soe::FrameStreamCuda']]],
   ['frame_5fa_5f',['frame_a_',['../structsoe_1_1_frame_stream.html#a9da8ba35b9130fb36559a99a3016b26a',1,'soe::FrameStream::frame_a_()'],['../structsoe_1_1_frame_stream_cuda.html#ae3014f4a48a76bc852cde1d3b9b75091',1,'soe::FrameStreamCuda::frame_a_()']]],
   ['frame_5fb_5f',['frame_b_',['../structsoe_1_1_frame_stream.html#ab0c76308823ad7c544ab476fe15346bd',1,'soe::FrameStream::frame_b_()'],['../structsoe_1_1_frame_stream_cuda.html#a7e7b353da094dabd208b1ec8c5e4b1ae',1,'soe::FrameStreamCuda::frame_b_()']]],
   ['frames_5fcount_5f',['frames_count_',['../structsoe_1_1_frame_stream.html#a001acb33961bb2e476ba25768a11070a',1,'soe::FrameStream::frames_count_()'],['../structsoe_1_1_frame_stream_cuda.html#a589c7bbb37394eafb214a43ad3d7c29d',1,'soe::FrameStreamCuda::frames_count_()']]],
