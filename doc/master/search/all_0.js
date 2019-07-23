6aa617692efebc0d3e5349c1206b5b1d16243db3
ci <ci>
2019-07-23 00:08:46 +0000

    [ci] update 'doc/master' commit 7b2b78ca2552721cfbe89184549dd0691d0088d4

--- doc/master/search/all_0.js
+++ doc/master/search/all_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['bind_5ffront',['bind_front',['../namespaceutils.html#a8495995f058cf7ab4ca134e12c60065f',1,'utils']]],
-  ['bind_5ffront_2eh',['bind_front.h',['../bind__front_8h.html',1,'']]]
+  ['bind_5ffront_2eh',['bind_front.h',['../bind__front_8h.html',1,'']]],
+  ['bind_5ffront_2etest_2ecpp',['bind_front.test.cpp',['../bind__front_8test_8cpp.html',1,'']]]
 ];
--- doc/master/search/files_0.js
+++ doc/master/search/files_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['bind_5ffront_2eh',['bind_front.h',['../bind__front_8h.html',1,'']]]
+  ['bind_5ffront_2eh',['bind_front.h',['../bind__front_8h.html',1,'']]],
+  ['bind_5ffront_2etest_2ecpp',['bind_front.test.cpp',['../bind__front_8test_8cpp.html',1,'']]]
 ];
